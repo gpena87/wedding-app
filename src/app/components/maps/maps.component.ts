b90c5adf3454d02afecd9fd058a6acec1639088a
gpena87 <gpenagutierrez87@gmail.com>
2026-09-15 22:07:49 -0300

    fix

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -7,7 +7,7 @@ import { Component, signal } from '@angular/core';
 })
 export class MapsComponent {
   title = signal('Ceremonia');
-  addressDetail = signal('Av. Las Condes 9700, Las Condes, Santiago de Chile');
-  addressDescription = signal('Parroquia Santa Maria de Las Condes');
-  addressLink = signal('https://maps.app.goo.gl/BZm533e8FkiRVdYp7');
+  addressDetail = signal('Av. la Compañía s/n esq. Las Delicias\nCodegua, Rancagua — VI Región');
+  addressDescription = signal('Casona Las Delicias');
+  addressLink = signal('https://maps.app.goo.gl/7MauQC4rtDqfdtmk8');
 }
